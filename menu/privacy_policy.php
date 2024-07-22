e53314e4a7f6a63a0a6922165dc5696af27b34e0
Abraham Livinus <60428819+chidopraise@users.noreply.github.com>
2024-07-22 15:32:18 +0100

    update

--- menu/privacy_policy.php
+++ menu/privacy_policy.php
@@ -2,13 +2,13 @@
 session_start();
 	class page_info{
 		public $page_id = 2;
-		public $page_name = "geolocate";
+		public $page_name = "privacy policy";
 	}
 	include("../lib/control.php");
 	include("../parts/start.php");
 	include("../parts/head.php");
 	//include("../parts/shoulder.php");
-	include("../parts/geolocate.php");
+	include("../parts/privacy_policy.php");
 	//include("../parts/lap.php");
 	include("../parts/leg.php");
 	include("../parts/stop.php");
